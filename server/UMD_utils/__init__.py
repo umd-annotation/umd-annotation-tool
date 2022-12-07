9e645b21ed9eb7846932ab90e160520ef22c32d0
Bryon Lewis <Bryon.Lewis@kitware.com>
2022-12-07 15:22:05 -0500

    removing extras

--- server/UMD_utils/__init__.py
+++ server/UMD_utils/__init__.py
@@ -2,7 +2,7 @@ from typing import Any, Dict, List, Optional, Tuple
 
 from girder.utility import ziputil
 
-from UMD_utils import UMD_export, types
+from UMD_utils import UMD_export
 
 TRUTHY_META_VALUES = ['yes', '1', 1, 'true', 't', 'True', True]
 FALSY_META_VALUES = ['no', '0', 0, 'false', 'f', 'False', False]
